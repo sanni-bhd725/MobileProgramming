9ad29c42dd2f917f7eafcbdcb2994466a7cd29ba
sanni-bhd725 <sanni.nevala@myy.haaga-helia.fi>
2024-11-27 20:32:16 +0200

    Tab navigation finished

--- MyBookshelf/screens/SignOut.js
+++ MyBookshelf/screens/SignOut.js
@@ -1,31 +1,8 @@
-import React, { useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { useEffect } from 'react';
 import { FIREBASE_AUTH } from '../firebaseConfig';
 
 export default function SignOut({ navigation }) {
     useEffect(() => {
-        // Suoritetaan uloskirjautuminen, kun tämä komponentti avataan
         FIREBASE_AUTH.signOut()
-            .then(() => {
-                // Ohjataan takaisin kirjautumisnäkymään
-                navigation.replace('Login');
-            })
-            .catch((error) => {
-                console.error('Sign-out error:', error);
-            });
     }, [navigation]);
-
-    return (
-        <View style={styles.container}>
-            <Text>Signing out...</Text>
-        </View>
-    );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-});
